62876ea8f109c3a0717b511d7b5f4ef890b02ef3
onurcanalpp <onurcanalpp@gmail.com>
2021-06-01 18:17:53 +0300

    react project created

---
+++ user-app/src/App.js
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+    const title = "User App";
+    const isAuthorized = false;
+    
+    
+  return (
+    <div className="container">
+        <h4>User App</h4>
+        <hr />
+    </div>
+  );
+}
+
+export default App;
